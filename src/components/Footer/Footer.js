32f1755a5427ec823a5fb9002387b66fd6d22851
joshkent94 <josh.kent94@yahoo.co.uk>
2021-12-03 15:20:35 +0000

    Add footer content

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,9 +1,29 @@
 import './Footer.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCopyright, faHeart } from '@fortawesome/free-solid-svg-icons';
+import React from 'react';
 
 export default function Footer() {
     return (
-        <div id="footer">
-            Footer
-        </div>
+        <React.Fragment>
+            <div id="upper-footer">
+                <div id="upper-footer-content">
+                    <div id="upper-footer-left">
+                        <h5 className="footer-heading">Features</h5>
+                        <p><a href="#suggestion-feature" className="footer-links">Song suggestions</a></p>
+                        <p><a href="#gigs-feature" className="footer-links">Gig information</a></p>
+                        <p><a href="#forum-feature" className="footer-links">Music forum</a></p>
+                    </div>
+                    <div id="upper-footer-right">
+                        <button id="footer-sign-up-button" className="coolBeans"><a href="https://app.shutupanddance.io/signup">START FOR FREE</a></button>
+                    </div>
+                </div>
+            </div>
+            <div id="lower-footer">
+                <div id="lower-footer-content">
+                    <p id="copyright">Made with <FontAwesomeIcon icon={faHeart} /> by Shut Up And Dance Ltd. <FontAwesomeIcon icon={faCopyright} /> All rights reserved.</p>
+                </div>
+            </div>
+        </React.Fragment>
     );
 };
